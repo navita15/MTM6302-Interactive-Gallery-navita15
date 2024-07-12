a0ef5a0938b58443160def62bc35ab9a7feabb83
navita15 <133902865+navita15@users.noreply.github.com>
2024-07-12 16:02:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const images= {
+    {src: 'images/1.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/2.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/3.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/4.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/5.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/6.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/7.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/8.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/9.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/10.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/11.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/12.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/13.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/14.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/15.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/16.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/17.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/18.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/19.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+     {src: 'images/.jpg , hdSrc:'images/hd/.jpg',caption: 'First Image'},
+    }
